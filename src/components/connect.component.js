d2c90e1a0e99a07278cfb41c4ddbaf446ca945b1
Jaskiratpal <jaskiratpal@mail.fresnostate.edu>
2019-05-01 21:58:34 -0700

    Initial Commit

---
+++ src/components/connect.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Connect extends Component {
+  render()
+  {
+    return (
+      <div>
+        <p>Welcome to the Connect page.</p>
+      </div>
+    )
+  }
+}
---
+++ src/components/my-info.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class MyInfo extends Component {
+  render()
+  {
+    return (
+      <div>
+        <p>Welcome to my Portfolio.</p>
+      </div>
+    )
+  }
+}
