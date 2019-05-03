8c1977472cd8d1729cebc33cbc37de636f2a8999
Jaskiratpal <jaskiratpal@mail.fresnostate.edu>
2019-05-03 01:05:56 -0700

    Main page progress
    Added banner, about me, education and projects

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'font-awesome/css/font-awesome.min.css';
+import {Helmet} from "react-helmet";
 
 import MyInfo from "./components/my-info.component";
 import Connect from "./components/connect.component";
 import ProjectList from "./components/project-list.component";
 
+var FontAwesome = require('react-fontawesome');
 //import logo from "./logo.png";
 class App extends Component {
   render() {
     return (
       <Router>
+        <Helmet>
+          <title>Jas Singh</title>
+          <link rel="icon" href="./images/avatar-icon.png"></link>
+        </Helmet>
         <nav className="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
           <div className="container">
             <a className="navbar-brand" href="/">J.S</a>
@@ -22,16 +29,30 @@ class App extends Component {
                 <Link to="/projects" className="nav-link">Projects</Link>
               </li>
               <li className="nav-item navContact">
-                <Link to="/connect" className="nav-link">Contact</Link>
+                <Link to="/contact" className="nav-link">Contact</Link>
               </li>
             </ul>
+            <div>
+              <a href="https://www.linkedin.com/in/jaskiratpal-singh-6a1812151/"><FontAwesome className="faIcons" name="linkedin" size="lg" /></a>
+              <a href="https://github.com/Jaskiratpal"><FontAwesome className="faIcons" name="github" size="lg" /></a>
+              <a href="https://drive.google.com/open?id=1maAGaTbIeyq6fSM1rVulox05yAI-21pR"><FontAwesome className="faIcons" name="sticky-note" size="lg" /></a>
+            </div>
+
           </div>
         </nav>
-        <div className="container">
+        <div className="">
           <Route path="/" exact component={MyInfo} />
           <Route path="/projects" component={ProjectList} />
-          <Route path="/connect" component={Connect} />
+          <Route path="/contact" component={Connect} />
         </div>
+        <br></br>
+        <footer className="page-footer font-small" >
+
+          <div className="footer-copyright text-center py-3">© 2018 Copyright
+    <a href="https://mdbootstrap.com/education/bootstrap/"></a>
+          </div>
+
+        </footer>
       </Router>
 
     );
--- src/components/my-info.component.js
+++ src/components/my-info.component.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react';
 
 export default class MyInfo extends Component {
-  render()
-  {
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  render() {
     return (
       <div>
-        <p>Welcome to my Portfolio.</p>
+        <div className="jumbotron jumbotron-fluid mainJumbo">
+          <div className="container">
+            <h1 className="display-4">Jaskiratpal Singh</h1>
+            <p className="lead">A Computer Science graduate who loves the art of Software Engineering and aspires to learn.</p>
+            <a className="btn btn-primary btn-lg buttonLinks" href="/projects" role="button">Projects</a>
+            <a className="btn btn-primary btn-lg buttonLinks" href="/contact" role="button">Contact</a>
+
+          </div>
+        </div>
+        <div className="container">
+          <h3>About Me</h3>
+          <hr className="my-4"></hr>
+          <p>Hello! I'm Jas, a Computer Science graduate situated near San Francisco, CA. I love learning new technologies and my primary interest is full-stack development. I developed this website using the MERN stack and you may view my projects by using the projects link.
+          </p>
+          <p>If you would like to reach out to me then please click the "contact" link and I'll be in touch with you ASAP.
+          </p>
+          <br></br>
+          <h3>Education</h3>
+          <hr className="my-4"></hr>
+          <div class="row mainEducation">
+            <div className="col-sm-10">
+              <h5>California State University of Fresno</h5>
+              <p>B.S in Computer Science </p>
+            </div>
+            <div className="col-sm-2 bulldogsLogo">
+            </div>
+          </div>
+          <br></br>
+          <h3>Projects</h3>
+          <hr className="my-4"></hr>
+          <div class="card-deck">
+            <div class="card">
+            <a href="https://github.com/Jaskiratpal/myathelite"><img class="card-img-top" src="images/myathelite.png" alt="Card image cap" /></a>
+              <div class="card-body">
+                <a href="https://github.com/Jaskiratpal/myathelite" ><h5 class="card-title">MyAthelite</h5></a>
+                <h5>Full-Stack</h5>
+                <p class="card-text">A web application built for athletes. Allows users to upload stats tailored for sports of choice and puts them in a database to be viewed by others. Features status messages and communication.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">HTML, CSS, Express.js, MongoDB, AJAX, JQuery, Express Handlebars, Materialize CSS</small>
+              </div>
+            </div>
+            <div class="card">
+            <a href="https://github.com/Jaskiratpal/picross"><img class="card-img-top" src="images/picross.png" alt="Card image cap" /></a>
+              <div class="card-body">
+                <a href="https://github.com/Jaskiratpal/picross"><h5 class="card-title">Picross</h5></a>
+                <h5>Back-End & Game Logic</h5>
+                <p class="card-text">A puzzle game also know as nonogram where the goal is to discover all the tiles with the least amount of mistakes. Features multiple game modes, time trackers and a hi-score board.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">JavaScript, HTML, CSS, PHP, AJAX, JQuery, MySQL</small>
+              </div>
+            </div>
+            <div class="card">
+            <a href="https://github.com/Jaskiratpal/BFSMaze"><img class="card-img-top" src="images/card-img.png" alt="Card image cap" /></a>
+              <div class="card-body">
+                <a href="https://github.com/Jaskiratpal/BFSMaze"><h5 class="card-title">Maze Game</h5></a>
+                <h5>Full-Stack</h5>
+                <p class="card-text">A maze game where the goal is to acquire the magical treasure chest before being consumed by enemy bats. Enemy AI pathing done using Breadth-first search.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">C++, OpenGL</small>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
